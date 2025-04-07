58ac403bdc03b90bb54eed523223f2fffaf061b3
nareshb7 <nareshbjava7@gmail.com>
2025-04-07 14:42:00 +0530

    project setup using webpack

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js', // Output file name
+    publicPath: "/"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-react",
+              "@babel/preset-env",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },{
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/404.html',
+      filename: '404.html',
+      inject: false,  // Prevents Webpack from injecting script tags into 404.html
+    }),
+  ],
+  devServer: {
+    port: 8080,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+};
